e1fb03e4b2ac4089054585d889956ebafd66a3e3
Anurag Rathore <ranurag378@gmail.com>
2024-10-20 21:57:36 +0000

    added jwt remaining part

---
+++ userservice/src/main/java/com/droppart/userservice/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.droppart.userservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.droppart.userservice.dao.BookRepository;
+import com.droppart.userservice.model.Book;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+    @Autowired
+	private BookRepository repository;
+
+	@PostMapping
+	public Book saveBook(@RequestBody Book book){
+		return repository.save(book);
+	}
+
+	@GetMapping
+	public List<Book> getBooks(){
+		return repository.findAll();
+	}
+}
