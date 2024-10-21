f2eb483ea21785f0307f83f351927082a8bcc1ec
Anurag Rathore <ranurag378@gmail.com>
2024-10-21 22:38:11 +0000

    added services

--- gateway/src/main/java/com/droppart/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/droppart/gateway/GatewayApplication.java
@@ -1,16 +1,15 @@
-package com.droppart.userservice;
+package com.droppart.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserServiceApplication {
-	
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
